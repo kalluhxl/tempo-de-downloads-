3b0829b0abf4be193f51f04f21e5bf5ea60d0dc1
Ana Carolina <kalluhxl@gmail.com>
2026-04-24 11:01:15 -0300

    tempo estimado

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿double fileSizeMB = 50000;
+double speedMbps = 10;
+double speedMBs = speedMbps / 8;
+double timeInSeconds = fileSizeMB / speedMBs;
+TimeSpan time = TimeSpan.FromSeconds (timeInSeconds);
+Console.WriteLine($"tempo estimado: {time.Minutes}m {time.Seconds}s");
